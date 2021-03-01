9e7f98f2755d0428618dc84e45dade2e97768b3c
pablo <paulandrewjeffrey@gmail.com>
2021-02-28 16:15:59 -0800

    made a timer

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,39 @@
-document.getElementById("start").addEventListener('click', function() {
-    document.getElementById('quiztext').innerText = 'lol';
-})
+const startButton = document.getElementById('start')
+const clockTime = document.getElementById('clock-time')
+const quizText = document.getElementById('quiztext')
+startButton.addEventListener('click', kickoff)
+
+
+ function clock() {
+    let time = 12;
+    let countdown = setInterval(() => {
+        if (time >= 10) {
+            clockTime.innerText = time;
+            console.log(time);
+            time--;
+        } else if (time <= 9 && time != 0) {
+            clockTime.innerText = '0'+ time;
+            console.log(time);
+            time--;        
+        } else {
+            time = 0;
+            clockTime.innerText = '00';
+            console.log('fuck');
+            clearInterval(countdown);
+            }
+    }, 1000)
+}
+
+function kickoff() {
+    startButton.style.visibility='hidden';
+    quizText.innerText = 'You will have 60 seconds to answer some questions... \nWrong answers will cost you time and self-esteem. \nGood luck!';
+    setTimeout(clock, 1000);
+    clearTimeout(kickoff);
+}
+
+
+
+// function scorekeeper()
+
+// function quiz()
+
